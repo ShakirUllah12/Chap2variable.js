8e849ed7b7b06804f8c44ce99b57c9a55326bb59
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-13 16:16:14 +0500

    chap2 done

---
+++ script.js
@@ -0,0 +1,46 @@
+var myName="shakir";
+alert(myName);
+
+let myFatherName="said nabi khan";
+console.log(myFatherName);
+
+const myBook="physics";
+console.log(myBook);
+
+var myFavourit="cheacken";
+console.log(myFavouri);
+
+let myDream="webdeveloper";
+console.log(myDream);
+
+if (1==1){
+alert("han g sai hai")
+};
+
+if (1==1){
+    alert("barabar hai")
+}
+else{
+    alert("nahi barabar hai")
+};
+
+if (1===1){
+    alert("barabar hai")
+}
+else{
+    alert("nahi barbar hai")
+};
+
+if (1==="1"){
+    alert("correct hai")
+}
+else{
+    alert("nahi correct hai")
+};
+
+== equal is only cheack value  
+ but === equal to cheack value and data type
+
+x = 5;
+y = 6;
+z = x + y;
